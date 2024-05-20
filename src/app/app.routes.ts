cd7f71edac8750297e8fc8653d208e4dd42fdacf
Talita Henriques <rochatalita86@gmail.com>
2024-05-20 08:27:38 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
+import { AdminComponent } from './components/pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/login',
+        component: LoginComponent
+    },
+    {
+        path: 'pages/admin',
+        component: AdminComponent
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/pages/login'
+    }
+
+];
