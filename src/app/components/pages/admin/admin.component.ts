fb12840bf22eccfec738a9e0c3c795fefaf2fac7
Talita Henriques <rochatalita86@gmail.com>
2024-05-20 12:59:10 -0300

    update do projeto

--- src/app/components/pages/admin/admin.component.ts
+++ src/app/components/pages/admin/admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
